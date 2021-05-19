ef3705276d6e94d2d2d0fcf8fba6340728fedbd7
Jensengbg-Henrik-Johansson <54744154+Rokakerman@users.noreply.github.com>
2021-05-19 14:44:45 +0200

    Saving a user succesfull

--- pages/Login.vue
+++ pages/Login.vue
@@ -31,7 +31,7 @@
         <input type="checkbox" class="form-check-input" id="exampleCheck1" />
         <label class="form-check-label" for="exampleCheck1">Check me out</label>
       </div>
-      <button type="submit" @click="getRequest()" class="btn btn-primary">
+      <button type="submit" @click="createUser()" class="btn btn-primary">
         Submit
       </button>
     </form>
@@ -48,22 +48,22 @@ export default {
   },
   methods: {
     async getRequest() {
-      let id = process.env.CLIENT_ID
-      console.log(id)
-      // Do API stuff
-      let url =
-        'https://api.jamendo.com/v3.0/oauth/authorize?client_id=a31f0360&redirect_uri=/'
+      // let id = process.env.CLIENT_ID
+      // console.log(id)
+      // // Do API stuff
+      // let url =
+      //   'https://api.jamendo.com/v3.0/oauth/authorize?client_id=a31f0360&redirect_uri=/'
 
-      const response = await fetch(url, {
-        method: 'GET',
-        headers: {
-          'CONTENT-TYPE': 'application/json',
-        },
-        mode: 'no-cors',
-      })
-      let readable = await response.json()
-      console.log(readable)
-      return readable
+      // const response = await fetch(url, {
+      //   method: 'GET',
+      //   headers: {
+      //     'CONTENT-TYPE': 'application/json',
+      //   },
+      //   mode: 'no-cors',
+      // })
+      // let readable = await response.json()
+      // console.log(readable)
+      // return readable
 
       //  .then(console.log(respone => response.json()))
       //  .then(response => console.log(data))
@@ -90,6 +90,50 @@ export default {
       //     // always executed
       //     });
       // }
+    },
+    async createUser() {
+      try {
+        console.log("inside the try")
+        await this.$fire.auth.createUserWithEmailAndPassword(
+          this.email,
+          this.password
+        )
+      } catch (e) {
+        console.log(e)
+      }
+    },
+
+    //FireStore
+
+    //Get data from db test an document test
+    async getData() {
+      const db = this.$fire.firestore
+        .collection('test')
+        .doc('GJhjnGSWINKcHjMsnUrj')
+      try {
+        const doc = await db.get()
+        console.log(doc.data())
+      } catch (e) {
+        console.error(e)
+      }
+    },
+
+    // Add Document
+    async addUser() {
+      const db = this.$fire.firestore.collection('users')
+      const x = await db.get()
+      console.log('Inside addUser')
+      this.getAllData()
+      await db.doc('UUID').set({
+        username: 'Markus',
+      })
+    },
+
+    // Get data from db test
+    async getAllData() {
+      const snapshot = await this.$fire.firestore.collection('users').get()
+      const readable = snapshot.docs.map((doc) => doc.data())
+      console.log(readable)
     },
   },
 }
