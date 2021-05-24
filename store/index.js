5133c4409ee36feb85857d0899e786803d842cd0
Jensengbg-Henrik-Johansson <54744154+Rokakerman@users.noreply.github.com>
2021-05-24 17:30:35 +0200

    newUser store action update
    - Added checks to see if any existing documents in contain matching username or email
    
    - Returns alert if matches found

--- store/index.js
+++ store/index.js
@@ -3,27 +3,41 @@ import Vuex from 'vuex'
 
 Vue.use(Vuex)
 
-export const store = new Vuex.Store({
-  state: {
-    count: 0,
-  },
-  mutations: {
-    increment(state) {
-      state.count++
-    },
-  },
-  actions: {
-    async writeToFirestore() {
-      const messageRef = this.$fire.firestore.collection('test').doc('message')
-      try {
-        await messageRef.set({
-          message: 'Nuxt-Fire with Firestore rocks!',
-        })
-      } catch (e) {
-        alert(e)
-        return
-      }
-      alert('Success.')
-    },
-  },
+export const state = () => ({
+  counter: 0,
 })
+
+export const actions = {
+  async writeToFirestore() {
+    return console.log('hi')
+  },
+  async newUser(context, params) {
+    let cancelOperation = false
+    const ref = this.$fire.firestore.collection('users')
+    const matchingEmails = await ref.where('email', '==', params.email).get()
+    const matchingUserNames = await ref.where('displayName', '==', params.userName).get()
+    if (matchingEmails.empty && matchingUserNames.empty) {
+      console.log("No matching documents")
+    } else {
+      cancelOperation = true
+      console.log("Found matching documents")
+    }
+
+    try {
+      if (!cancelOperation) {
+        console.log(cancelOperation)
+        console.log(params) 
+        const data = {
+          email: params.email,
+          displayName: params.userName,
+          library: { likedTracks: [], likedPlaylists: {}, playlists: {} },
+        }
+        await ref.add(data)
+      } else {
+        return alert("username or email is already taken")
+      }
+    } catch (e) {
+      return Promise.reject(e)
+    }
+  },
+}
