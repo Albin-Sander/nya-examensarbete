aee820e399d424a0b75e879a88ee2f81e1b05590
TheNeonLeon <leon.listo@hotmail.com>
2021-05-25 11:39:50 +0200

    remove old code
    removed unnecessary old code

--- pages/Login.vue
+++ pages/Login.vue
@@ -1,38 +1,6 @@
 <template>
   <div>
-    <form v-on:submit.prevent="getRequest">
-      <div class="form-group">
-        <label for="exampleInputEmail1">Email address</label>
-        <input
-          type="email"
-          v-model="email"
-          class="form-control"
-          id="exampleInputEmail1"
-          aria-describedby="emailHelp"
-          placeholder="Enter email"
-          required
-        />
-        <small id="emailHelp" class="form-text text-muted"
-          >We'll never share your email with anyone else.</small
-        >
-      </div>
-      <div class="form-group">
-        <label for="exampleInputPassword1">Password</label>
-        <input
-          type="password"
-          v-model="password"
-          class="form-control"
-          id="exampleInputPassword1"
-          placeholder="Password"
-          required
-        />
-      </div>
-      <div class="form-check">
-        <input type="checkbox" class="form-check-input" id="exampleCheck1" />
-        <label class="form-check-label" for="exampleCheck1">Check me out</label>
-      </div>
-      <button type="submit" @click="getRequest()" class="btn btn-primary">Submit</button>
-    </form>
+
   </div>
 </template>
 
@@ -40,8 +8,7 @@
 export default {
     data: () => {
         return{
-            email: "",
-            password: ""
+          
         }    
     },
     
