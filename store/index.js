efa415fe9a0fcfb2041249cac4808d0a04d496e0
Jensengbg-Henrik-Johansson <54744154+Rokakerman@users.noreply.github.com>
2021-05-25 11:08:29 +0200

    added a commented function for myself

--- store/index.js
+++ store/index.js
@@ -44,3 +44,44 @@ export const actions = {
     }
   },
 }
+
+// async newUser(context, params) {
+//   return (
+//     new Promise( async (resolve, reject) => {
+//       const matchingEmails = await ref
+//         .where('email', '==', params.email)
+//         .get()
+//       const matchingUserNames = await ref
+//         .where('displayName', '==', params.userName)
+//         .get()
+//       if (matchingEmails.empty && matchingUserNames.empty) {
+//         console.log('No matching documents')
+//       } else {
+//         cancelOperation = true
+//         console.log('Found matching documents')
+//         throw error
+//       }
+
+//       try {
+//         // Checks have been resolved and should work, test additionaly
+//         if (!cancelOperation) {
+//           console.log(cancelOperation)
+//           console.log(params)
+//           const data = {
+//             email: params.email,
+//             displayName: params.userName,
+//             library: { likedTracks: [], likedPlaylists: {}, playlists: {} },
+//           }
+//           await ref.add(data)
+//         } else {
+//           return alert('username or email is already taken')
+//         }
+//       } catch (e) {
+//         return Promise.reject(e)
+//       }
+//     }),
+//     (error) => {
+//       reject(error)
+//     }
+//   )
+// },
