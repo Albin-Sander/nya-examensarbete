015b94350a2ad1690b09699c4264d611243d8d90
Jensengbg-Henrik-Johansson <54744154+Rokakerman@users.noreply.github.com>
2021-05-26 14:32:47 +0200

    Updated store
    - Made two separate vuex actions, one for checking credentials and one for writing to firestore.

--- store/index.js
+++ store/index.js
@@ -8,45 +8,52 @@ export const state = () => ({
 })
 
 export const mutations = {
-  setCredentialsExist (state) {
-    console.log('hi')
+  setCredentialsExist(state) {
     state.credentialsExist = true
   },
-  setCredentialsDoesNotExist (state) {
-    console.log('hi')
+  setCredentialsDoesNotExist(state) {
     state.credentialsExist = false
-  }
+  },
 }
 
 export const actions = {
   async writeToFirestore() {
     return console.log('hi')
   },
-  async newUser(context, params) {
+
+  async checkUserCredentials(context, params) {
     console.log(context)
-    console.log("--------------------------------------")
+    console.log('--------------------------------------')
     console.log(state)
-    console.log("--------------------------------------")
-    let cancelOperation = false
-    const ref = this.$fire.firestore.collection('users')
+    console.log('--------------------------------------')
+    const ref = await this.$fire.firestore.collection('users')
 
-    // Checks have been resolved and should work, test additionaly
-    
+    // This a action that checks if the credentials already exist or not without actually 
+    // writing anything to firestore, this is to prevent creating a user object in firestore
+    // incase the firebase auth throws "weak password" error upon calling createUser function
+
     const matchingEmails = await ref.where('email', '==', params.email).get()
-    const matchingUserNames = await ref.where('displayName', '==', params.userName).get()
+    const matchingUserNames = await ref
+      .where('displayName', '==', params.userName)
+      .get()
     if (matchingEmails.empty && matchingUserNames.empty) {
-      console.log("No matching documents")
-      context.commit("setCredentialsDoesNotExist")
+      console.log('No matching documents')
+      await context.commit('setCredentialsDoesNotExist')
+      return
     } else {
-      cancelOperation = true
-      console.log("Found matching documents")
-      context.commit("setCredentialsExist")
+      console.log('Found matching documents')
+      context.commit('setCredentialsExist')
+      return
     }
+  },
 
-    try { // Checks have been resolved and should work, test additionaly
+  async addNewUser(context, params) {
+    const ref = this.$fire.firestore.collection('users')
+    try {
       console.log(context.state.credentialsExist)
       if (!context.state.credentialsExist) {
-        console.log(params) 
+        console.log("Inside if in addNewUser action")
+        console.log(params)
         const data = {
           email: params.email,
           displayName: params.userName,
@@ -54,7 +61,7 @@ export const actions = {
         }
         await ref.add(data)
       } else {
-        return 
+        return
       }
     } catch (e) {
       return Promise.reject(e)
