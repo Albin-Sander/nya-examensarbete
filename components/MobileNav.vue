8a1b3f044a3defd672579eb61e5598eeaf95a930
Jensengbg-Henrik-Johansson <54744154+Rokakerman@users.noreply.github.com>
2021-05-27 16:23:51 +0200

    create mobile nav

---
+++ components/MobileNav.vue
@@ -0,0 +1,21 @@
+<template>
+    <nav class="navbar-mobile">
+        hello
+    </nav>
+</template>
+
+<script>
+    export default {
+        
+    }
+</script>
+
+<style  scoped>
+.navbar-mobile {
+    position: fixed;
+    height: 3rem;
+    width: 100vw;
+    background-color: green;
+    bottom: 0;
+}
+</style>
