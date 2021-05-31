41f0b65983a248c3c6156e29ea6c646cbe4a179b
Albin Sander <51294735+Albin-Sander@users.noreply.github.com>
2021-05-31 12:31:59 +0200

    Added some fixes to nav desktop

---
+++ components/Nav_desktop.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    <ul class="nav justify-content-center">
+      <li class="nav-item">
+        <a class="nav-link active" aria-current="page">
+          <NuxtLink to="/">Home</NuxtLink>
+        </a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link">
+          <NuxtLink to="/login">Library</NuxtLink>
+        </a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link">
+          <NuxtLink to="/login">Log in</NuxtLink>
+        </a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true"
+          >Disabled</a
+        >
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style></style>
