970dee14e7e9ce8f0b091054bc9e6521e1116f6a
Albin Sander <51294735+Albin-Sander@users.noreply.github.com>
2021-06-01 17:11:39 +0200

    Added html and css for musicplayer component

---
+++ components/MusicPlayer.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="musicplayer">
+    <img src="~/assets/previoussong.svg" />
+    <img :src="playButton" />
+    <img src="~/assets/nextsong.svg" />
+  </div>
+</template>
+
+<script>
+import playButton from '/assets/playbutton.svg'
+import pauseButton from '/assets/pauseButton.svg'
+export default {
+  data() {
+    return {
+      playButton: playButton,
+      pauseButton: pauseButton,
+    }
+  },
+}
+</script>
+
+<style scoped>
+.musicplayer {
+  background-color: white;
+  width: 50rem;
+  height: 3.5rem;
+  display: flex;
+  justify-content: space-around;
+  border-radius: 2rem;
+  margin-bottom: 5rem;
+}
+
+img:hover {
+  cursor: pointer;
+}
+</style>
