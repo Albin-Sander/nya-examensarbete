1d77f74ffdda6ff1f75d60335fbacd7145f1d34b
Jensengbg-Henrik-Johansson <54744154+Rokakerman@users.noreply.github.com>
2021-06-01 11:16:49 +0200

    new library page

---
+++ pages/Library/index.vue
@@ -0,0 +1,35 @@
+<template>
+  <div></div>
+</template>
+
+<script>
+export default {
+  data: () => {
+    return {
+        user: {}
+    }
+  },
+  methods: {
+    async checkUser() {
+      let vm = this
+      await this.$fire.auth.onAuthStateChanged(async function (user) {
+        let obj
+        if (user) {
+          console.log(user.email)
+          obj = { user: true, email: await user.email }
+          console.log(obj)
+          return vm.user = obj
+        } else {
+          console.log('no user')
+          return
+        }
+      })
+    },
+  },
+  mounted() {
+      this.checkUser()
+  }
+}
+</script>
+
+<style lang="scss" scoped></style>
