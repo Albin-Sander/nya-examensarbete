5711a053896bb8a14c14a2fb1a87cc4e66aa11da
Jensengbg-Henrik-Johansson <54744154+Rokakerman@users.noreply.github.com>
2021-06-01 12:25:28 +0200

    Added functions to library page

--- pages/Library/index.vue
+++ pages/Library/index.vue
@@ -1,15 +1,20 @@
 <template>
-  <div></div>
+  <h1>Secret</h1>
 </template>
 
 <script>
+import { mapActions } from 'vuex'
 export default {
   data: () => {
     return {
-        user: {}
+        user: {},
+        library: {}
     }
   },
   methods: {
+      ...mapActions({
+      getUserLibrary: 'getUserLibrary',
+    }),
     async checkUser() {
       let vm = this
       await this.$fire.auth.onAuthStateChanged(async function (user) {
@@ -18,13 +23,26 @@ export default {
           console.log(user.email)
           obj = { user: true, email: await user.email }
           console.log(obj)
-          return vm.user = obj
+          vm.user = obj
+          await vm.getUserLibrary(obj)
+          return vm.getLibrary()
         } else {
           console.log('no user')
-          return
+          return (window.location.href = '/login')
         }
       })
     },
+    async getLibrary() {
+        let data = this.$store.state.userLibrary
+        this.library = data
+        console.log(data)
+    }
+  },
+  computed: {
+    //   async library() {
+    //       console.log("Library updated")
+    //       return this.library = this.$store.state.userLibrary
+    //   }
   },
   mounted() {
       this.checkUser()
